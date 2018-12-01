48b7a5beb7e63bd2f0ad24c06c17b6430014c172
Tomasz Jarych <tjarych@gmail.com>
2018-12-01 19:05:09 +0100

    UserDetails and mapper added

--- src/main/java/tj/udemy/User/mapper/UserMapper.java
+++ src/main/java/tj/udemy/User/mapper/UserMapper.java
@@ -1,21 +1,24 @@
 package tj.udemy.User.mapper;
 
-import org.mapstruct.InheritInverseConfiguration;
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import tj.udemy.User.domain.User;
 import tj.udemy.User.dto.UserDto;
+import tj.udemy.userdetails.mapper.UserDetailsMapper;
 
-@Mapper(componentModel="spring")
+@Mapper(componentModel="spring", uses=UserDetailsMapper.class)
 public interface UserMapper {
 
 	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+	@Mapping(ignore=true, target = "password")
 	UserDto userToUserDto(User user);
 	
-	@InheritInverseConfiguration
+	@Mapping(nullValuePropertyMappingStrategy=IGNORE, target = "password")
 	User userDtoToUser(UserDto dto);
-
+	
 }
